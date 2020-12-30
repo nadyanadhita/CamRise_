9122e5eac05ab50d8b73db1669038c428b8f5ec4
Nadya Nadhita <63330161+nadyanadhita@users.noreply.github.com>
2020-12-30 09:54:44 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+ public class Main {
+    public static void main(String[] args) {
+        CamRise camera = new CamRise();
+        new CurrentFingerprint(camera);
+      
+        camera.setFingerprint("106");
+    }
+}
